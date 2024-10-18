972122bb601c70b070328903627f095171367614
UtshadasCSE <utshachandradas@gmail.com>
2024-10-18 21:49:34 +0600

    Project updated

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// import css 
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
